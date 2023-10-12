7668ce0558997221f1546bfe3fcefdcbb2657fab
Sofya Borisenko <sofyaborisenko806@gmail.com>
2023-10-12 12:56:20 +0100

    done reviews

---
+++ src/data.js
@@ -0,0 +1,37 @@
+import person1 from "./assets/person-1.jpg";
+import person2 from "./assets/person-2.jpg";
+import person3 from "./assets/person-3.jpg";
+import person4 from "./assets/person-4.jpg";
+
+const reviews = [
+  {
+    id: 1,
+    name: "lily howard",
+    job: "photographer",
+    image: person1,
+    text: "I purchased the latest image editing software, and it's been a game-changer for my photography business. The intuitive interface and powerful features have significantly improved my workflow, making it a must-have tool for any photographer.",
+  },
+  {
+    id: 2,
+    name: "anna moss",
+    job: "graphic designer",
+    image: person2,
+    text: "I upgraded to a 4K monitor for graphic design, and the image clarity is astonishing. The color accuracy and sharpness of this display have made a noticeable difference in the quality of my work. Thank you for a great product.",
+  },
+  {
+    id: 3,
+    name: "josh ronaldson",
+    job: "web designer",
+    image: person3,
+    text: "The new image compression algorithm in this photo-sharing app is fantastic. It maintains image quality while reducing file size, which has made uploading and sharing pictures faster and more efficient. It's a lifesaver for those with limited data plans.",
+  },
+  {
+    id: 4,
+    name: "bill wu",
+    job: "photographer",
+    image: person4,
+    text: "I recently invested in a high-quality DSLR camera, and the image stabilization technology in the lens has exceeded my expectations. Even with shaky hands, I'm now able to capture incredibly sharp and steady shots, which has taken my photography to the next level.",
+  },
+];
+
+export default reviews;
